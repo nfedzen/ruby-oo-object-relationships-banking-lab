466f62552e9b3b2b1a5316a83d57608ddc9727ea
Nathan Fedzen <nfedzen@hotmail.com>
2020-12-15 15:44:32 -0700

    commit tree df8e1ddd6e9e63ad1873defacaea60c970f8dd1c

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,28 @@
 class Transfer
   # your code here
+  attr_reader :sender, :receiver, :amount, :status
+
+  def initialize sender, receiver, amount, status = 'pending'
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = status
+  end
+
+  def valid? 
+    if sender.valid? && receiver.valid?
+      true
+    else
+      false
+    end
+  end
+
+def execute_transaction
+  if @status = 'pending'
+    @sender.balance -= @amount
+    @receiver.balance += @amount
+    @status = 'complete'
+  end
+end
+
 end
